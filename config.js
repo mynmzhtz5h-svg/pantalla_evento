0d3cce2c359581fa99bfde0444c4a8abcdb360d4
Pablo Cerrutti <mynmzhtz5h@privaterelay.appleid.com>
2026-02-10 18:24:13 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   owner: "mynmzhtz5h-svg",
-  repo: "mynmzhtz5h-svg.github.io",
+  repo: "pantalla_evento",
   branch: "main",
   token: "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"
 };
